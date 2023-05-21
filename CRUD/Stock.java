d937ce064af0d15fe3fecb5825713851e803140e
JayManjrekar <jaymanjrekarapwork@gmail.com>
2023-05-20 23:14:00 -0700

    added crud functions

---
+++ CRUD/Stock.java
@@ -0,0 +1,10 @@
+package CRUD;
+
+public class Stock {
+    private int id;
+    private String name;
+    private float price;
+    private int quantity;
+
+    // constructors, getters and setters...
+}
