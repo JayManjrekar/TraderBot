e0fad09fc013c57a02588a56daef1f0998c3eb6f
Aryanboxout <emailaryantoday@gmail.com>
2023-05-22 22:32:20 -0700

    Create Tradelogic.java

---
+++ Tradelogic.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class StockTradingBot {
+
+    // Placeholder methods for API calls
+    private double getClosingPrice(String symbol, String date) {
+        // Replace with API call to get the closing price of the stock on the given date
+        return 0.0;
+    }
+  
+  
+  
