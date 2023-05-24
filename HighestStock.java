ad6117849b2e289aa68b389a927a9ddd14249876
Ritvik <ritvik@gmail.com>
2023-05-24 11:14:03 -0700

    starrt

---
+++ HighestStock.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class StockAPI {
+    private static final String API_URL = "https://api.example.com/stocks";
+
+    
+}
