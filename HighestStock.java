99fc023358f5aa8fa63b4e3b3c668fe1a3b2f549
Ritvik <ritvik@gmail.com>
2023-05-24 11:15:41 -0700

    higheststock.java need to implement API link

--- HighestStock.java
+++ HighestStock.java
@@ -7,5 +7,28 @@ import java.util.Scanner;
 public class StockAPI {
     private static final String API_URL = "https://api.example.com/stocks";
 
-    
+    public static String getStockData() throws IOException {
+        URL url = new URL(API_URL);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+        InputStream inputStream = conn.getInputStream();
+        Scanner scanner = new Scanner(inputStream);
+        StringBuilder response = new StringBuilder();
+        while (scanner.hasNextLine()) {
+            response.append(scanner.nextLine());
+        }
+        scanner.close();
+
+        return response.toString();
+    }
+
+    public static void main(String[] args) {
+        try {
+            String stockData = getStockData();
+            System.out.println(stockData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
